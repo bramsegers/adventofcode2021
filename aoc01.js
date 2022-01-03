aa05acc6a88338e3cf66192fc7238a2751a3cc57
Abraham Segers <bramsegers@users.noreply.github.com>
2022-01-03 05:49:44 +0100

    Delete aoc01.js

--- aoc01.js
+++
@@ -1,6 +0,0 @@
-let input = require('fs').readFileSync('input/aoc01.txt','utf8').split('\r\n').map(e=>+e)
-
-let f=(n)=>input.reduce((a,b,i)=>a+(b>input[i-n]),0)
-
-console.log(f(1))
-console.log(f(3))
